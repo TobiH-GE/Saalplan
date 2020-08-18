fa8b7868ffa18cb78b5aba62addc33415831f2af
tobias.hartwich <tobias.hartwich@hotmail.de>
2020-08-18 08:59:43 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Saalplan
+{
+    class Saalplan
+    {
+        Veranstaltung Veranstaltung;
+        Spielstätte Spielstätte;
+    }
+    class Veranstaltung
+    {
+        string Name;
+        double Datum;
+        double Zeit;
+    }
+    class Spielstätte
+    {
+        string Name;
+        string Anschrift;
+    }
+    class Platz
+    {
+        int Nummer;
+        int Reihe;
+    }
+    class Veranstaltungsplan
+    {
+        Saalplan Saalplan;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
