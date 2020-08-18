1ccd5708ca2a7f49afea31ccc8cd1ce55f51069c
tobias.hartwich <tobias.hartwich@hotmail.de>
2020-08-18 09:52:59 +0200

    Upload

--- Program.cs
+++ Program.cs
@@ -8,42 +8,43 @@ namespace Schauspielhaus
         Spielstätte Spielstätte;
         Platz[] AllePlaetze;
 
-        public void istFrei()
+        public bool istFrei(Platz Platz)
+        {
+            if (Platz.bestaetigt())
+                return false;
+            else
+                return true;
+        }
+        public void reservieren(Platz Platz)
         {
 
         }
-        public void reservieren()
+        public Spielstätte getSpielstaette()
         {
-
+            return Spielstätte;
         }
-        public void getSpielstaette()
+        public void setSpielstaette(Spielstätte Spielstätte)
         {
-
+            this.Spielstätte = Spielstätte;
         }
-        public void setSpielstaette()
+        public Veranstaltung getVeranstaltung()
         {
-
+            return Veranstaltung;
         }
-        public void getVeranstaltung()
+        public void setVeranstaltung(Veranstaltung Veranstaltung)
         {
-
+            this.Veranstaltung = Veranstaltung;
         }
-        public void setVeranstaltung()
+        public Platz[] getPlaetze()
         {
-
-
+            return AllePlaetze;
         }
-        public void getPlaetze()
+        public void setPlaetze(Platz[] AllePlaetze)
         {
-
-        }
-        public void setPlaetze()
-        {
-
-
+            this.AllePlaetze = AllePlaetze;
         }
     }
-        class Veranstaltung
+    class Veranstaltung
     {
         string Name;
         double Datum;
@@ -59,22 +60,25 @@ namespace Schauspielhaus
         int Nummer;
         int Reihe;
 
-        public void bestaetigt()
+        public bool bestaetigt()
         {
-            
+            if (Nummer != 0)
+                return true;
+            else
+                return false;
         }
     }
     class Veranstaltungsplan
     {
         Saalplan Saalplan;
 
-        public void getSaalplan()
+        public Saalplan getSaalplan()
         {
-
+            return Saalplan;
         }
-        public void setSaalplan()
+        public void setSaalplan(Saalplan Saalplan)
         {
-
+            this.Saalplan = Saalplan;
         }
     }
     class Program
